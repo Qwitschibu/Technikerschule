62ab4d535c4dec270927235d1f6f11a5e2565087
Daniel Haueis <>
2022-10-01 20:50:51 +0200

    upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kugeldberechnung_20220930
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Durchmesser = new int[6];
+
+            Durchmesser[1] = 2;
+            Durchmesser[2] = 5;
+            Durchmesser[3] = 10;
+            Durchmesser[4] = 20;
+            Durchmesser[5] = 40;
+            Durchmesser[6] = 100;
+
+
+            int Dichte = 7800;
+
+
+
+
+
+
+
+
+        }
+    }
+}
